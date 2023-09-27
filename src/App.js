f07af8417a677c8d844758fa793ad1ba8cd7d7cd
Rohit Hirulkar <rohithirulkar848384@gmail.com>
2023-09-27 10:32:41 +0530

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import "./App.css";
 import { Provider } from "react-redux";
 import { store } from "./store";
+
 import Header from "./components/Header";
-import { AllProductPage } from "./components/AllProducts";
+import React,{lazy,Suspense} from "react";
 import { ProductDetail } from "./components/ProductDetails";
 import { NotFound } from "./components/NotFound";
 import { Routes, Route } from "react-router-dom";
+const AllProdsLazily=lazy(()=>import("./components/AllProducts"))
 
 function App() {
   return (
     <Provider store={store}>
       <Header />
       <Routes>
-        <Route path="/" exact element={<AllProductPage/>} />
+        <Route path="/" exact element={<Suspense fallback="<h1>Loading.............</h1>"><AllProdsLazily /></Suspense>} />
         <Route path="/product/:productId" exact element={<ProductDetail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
